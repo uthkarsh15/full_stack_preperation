42e29f72a9ed64ea7780fad8368c108c9b345e9e
uthkarsh15 <shivamg1505@gmail.com>
2026-05-17 01:00:07 +0530

    react part 3

--- test/index.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById("addBtn").addEventListener("click", () => {
-    const a = Number(document.getElementById("a").value);
-    const b = Number(document.getElementById("b").value);
-
-    document.getElementById("result").textContent = a + b;
-});
